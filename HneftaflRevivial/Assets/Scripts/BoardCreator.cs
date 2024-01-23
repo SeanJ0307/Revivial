6b18f1d8be45ad356e126dd3f5311837c1fd655e
Sean Jenkins <104651351+SeanJ0307@users.noreply.github.com>
2024-01-23 15:24:59 -0800

    try to make a script template

--- HneftaflRevivial/Assets/Scripts/BoardCreator.cs
+++ HneftaflRevivial/Assets/Scripts/BoardCreator.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class BoardCreator : MonoBehaviour
 {
-    [Header("Prefab"), Tooltip("The tile prefab to spawn")]
-    public class Tile tilePrefab;
-
-    [Header("Boardsize"), Tooltip("The size of the board. Must be odd")]
-    public Vector2Int boardsize = new Vector2Int(11, 11);
-
-    public Material[] tileMaterials = new Material[3];
     // Start is called before the first frame update
     void Start()
     {
